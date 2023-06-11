f26db0eab253b22ef59662c4229e71b8cc6748c1
Srijan Mehrotra <Srijan.psn@gmail.com>
2023-06-11 22:25:21 +0530

    some progress

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
 from flask import Flask
 from application.database import db
-
+from application.config import LocalDevConfig
+from flask_migrate import Migrate
 app = None
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
+    '''
+    Creates the app
+    '''
+
+
     app = Flask(__name__, template_folder='templates')
+    app.config.from_object(LocalDevConfig)
     db.init_app(app)
+
+    migrate = Migrate(app, db)
+
+
     app.app_context().push()
+
+    
     return app
 
 
---
+++ application/config.py
@@ -0,0 +1,21 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config():
+    DEBUG = False
+    SQLITE_DB_DIR = None
+    SQLALCHEMY_DATABASE_URI = None
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class LocalDevConfig(Config):
+    DEBUG = True
+    SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(SQLITE_DB_DIR, "testdb.sqlite3")
+
+class ProdDevConfig(Config):
+    DEBUG = True
+    SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(SQLITE_DB_DIR, "proddb.sqlite3")
+
